466a7e971234b53fda985474637082bcced33ddc
szkyudi <iamsucchii@gmail.com>
2023-08-05 22:29:22 +0900

    都道府県一覧を表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Prefectures from './_components/Prefectures'
+
 export default function Home() {
-  return <h1>都道府県別の総人口推移グラフを表示するSPA</h1>
+  return (
+    <main>
+      <h1>都道府県別の総人口推移グラフを表示するSPA</h1>
+      <Prefectures />
+    </main>
+  )
 }
