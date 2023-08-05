4889f92d8aad9adf012966dcf8f166f43186c0b1
szkyudi <iamsucchii@gmail.com>
2023-08-05 13:14:25 +0900

    APIクライアントの定義

---
+++ src/lib/api/index.ts
@@ -0,0 +1,9 @@
+import { paths } from './schema'
+import createClient from 'openapi-fetch'
+
+export const api = createClient<paths>({
+  baseUrl: 'https://opendata.resas-portal.go.jp/api/v1',
+  headers: {
+    'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY!,
+  },
+})
